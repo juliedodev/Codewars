22e37325c5291a82b1dda5a415b4afb87da0d971
juliedodev <juliedo.dev@gmail.com>
2021-09-11 22:06:08 -0400

    Codewars

---
+++ kyu8/findmultiplesofanumber.js
@@ -0,0 +1,13 @@
+function findMultiples(integer, limit) {
+    //your code here
+    
+    const arr = [];
+    
+    for (let i = 1; i <= limit; i += 1) {
+      
+        if (integer * i <= limit) {
+          arr.push(integer * i);
+        }
+    }
+    return arr;
+  }

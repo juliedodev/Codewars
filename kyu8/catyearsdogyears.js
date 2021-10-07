a2e7c1ba664c230ec538c5d7b035fa212bb851f4
juliedodev <juliedo.dev@gmail.com>
2021-10-07 10:34:47 -0400

    codewars

---
+++ kyu8/catyearsdogyears.js
@@ -0,0 +1,7 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+  // Your code here!
+  if (humanYears === 1) {return [1, 15, 15];}
+  if (humanYears === 2) {return [2, 24, 24];}
+  
+  return [humanYears, 24+((humanYears-2)*4), 24+((humanYears-2)*5)];
+}

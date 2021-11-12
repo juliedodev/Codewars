ee069d954e8b5538561e5a403a16843c373f8b76
juliedodev <juliedo.dev@gmail.com>
2021-11-12 10:23:44 -0500

    codewars

---
+++ kyu8/oppositeNumber.js
@@ -0,0 +1,13 @@
+function opposite(number) {
+    if(number < 0){
+     number *= -1;
+     return number;
+    }
+    else if(number > 0){
+      number *= -1;
+      return number;
+    }
+    else if(number === 0){
+      return number;
+    }
+}

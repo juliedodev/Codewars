a77e69ad1aed30c91baaabac168f4daffe70798b
juliedodev <juliedo.dev@gmail.com>
2021-11-17 20:19:19 +0100

    petals

---
+++ kyu8/petals.js
@@ -0,0 +1,15 @@
+function howMuchILoveYou(nbPetals) {
+    let phrases = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+    ]
+    
+     return phrases[(nbPetals - 1) % phrases.length];
+ 
+  
+  
+}

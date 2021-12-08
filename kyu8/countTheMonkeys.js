6d5c6e1e59b3474c2d62f9beda2e1b9d16f1694b
juliedodev <juliedo.dev@gmail.com>
2021-12-08 10:34:54 -0500

    codewars

---
+++ kyu8/countTheMonkeys.js
@@ -0,0 +1,9 @@
+function monkeyCount(n) {
+    // your code here
+      let monkeyArr = []
+      for(let i = 1; i <= n; i++){
+        monkeyArr.push(i)
+        console.log(i,n,monkeyArr)
+      }
+      return monkeyArr
+    }
